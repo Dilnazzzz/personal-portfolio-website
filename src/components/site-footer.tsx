b6889b1cd3a2f640f33556e64ebe1856a2efeaf1
Dilnaz Baltabayeva <dilnaz@futre.me>
2026-01-27 19:45:06 -0800

    Initial scaffold

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { siteConfig } from '@/lib/site'
 
 export function SiteFooter() {
@@ -8,12 +7,11 @@ export function SiteFooter() {
       <div className="container flex flex-col items-center justify-between gap-4 md:flex-row">
         <p>&copy; {year} {siteConfig.name}. All rights reserved.</p>
         <div className="flex items-center gap-5">
-          <Link href={siteConfig.links.github} target="_blank" className="hover:text-zinc-900 dark:hover:text-white">GitHub</Link>
-          <Link href={siteConfig.links.twitter} target="_blank" className="hover:text-zinc-900 dark:hover:text-white">Twitter</Link>
-          <Link href={siteConfig.links.linkedin} target="_blank" className="hover:text-zinc-900 dark:hover:text-white">LinkedIn</Link>
+          <a href={siteConfig.links.github} target="_blank" rel="noreferrer" className="hover:text-zinc-900 dark:hover:text-white">GitHub</a>
+          <a href={siteConfig.links.twitter} target="_blank" rel="noreferrer" className="hover:text-zinc-900 dark:hover:text-white">Twitter</a>
+          <a href={siteConfig.links.linkedin} target="_blank" rel="noreferrer" className="hover:text-zinc-900 dark:hover:text-white">LinkedIn</a>
         </div>
       </div>
     </footer>
   )
 }
-
